01be5170e0eabc0d2b1b4d938a5d2ff2e055580e
Befuke <befuke.alexis@gmail.com>
2020-08-25 12:24:38 +0200

    Banking Project

---
+++ src/main/java/za/co/abcbank/display/Display.java
@@ -0,0 +1,17 @@
+package za.co.abcbank.display;
+
+public class Display{
+
+public void display(){
+
+System.out.println("WELCOME to ABC Bank");
+System.out.println("<<<< SELECT OPTION BELOW >>>>");
+System.out.println("1. Open Account");
+System.out.println("2. Deposit");
+System.out.println("3. Withdraw");
+System.out.println("4. Check Balance");
+System.out.println("5. Statements");
+System.out.println("6. More OPtion");                
+}
+
+}
